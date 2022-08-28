0b3709e65ee7ac6034e61d7df53644d1118f5682
Akatsuki <masahisakun@gmail.com>
2022-08-28 20:53:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,8 +31,9 @@ else:
     voicevox_key = os.environ['GEKKA_VOICEVOX_KEY']
     voicevox_speaker = "2"
 
-intents.message_content = True
+
 intents = discord.Intents.all()
+intents.message_content = True
 client = commands.Bot(command_prefix=prefix, intents=intents)
 with open('emoji_ja.json', encoding='utf-8') as file:
     emoji_dataset = json.load(file)
